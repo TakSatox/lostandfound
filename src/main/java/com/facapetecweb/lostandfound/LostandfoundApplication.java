b7d0c023fe6e0e5ea3b37d3d35a676e53d8eacd5
Carlos Sato <carlostakeshi100@gmail.com>
2024-10-24 23:41:41 -0300

    feature: config cors and openapi definition

--- src/main/java/com/facapetecweb/lostandfound/LostandfoundApplication.java
+++ src/main/java/com/facapetecweb/lostandfound/LostandfoundApplication.java
@@ -3,7 +3,11 @@ package com.facapetecweb.lostandfound;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
 @SpringBootApplication
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default server URL")})
 public class LostandfoundApplication {
 
 	public static void main(String[] args) {
